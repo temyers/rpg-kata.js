e377eca0bad1205a95a33d70093905201085d757
temyers <tim.myerscough@mechanicalrock.io>
2020-09-22 17:54:20 +0800

    step-4

--- features/combat.feature
+++ features/combat.feature
@@ -1,10 +1,20 @@
 Feature: Combat
 
   Rules:
-    - Characters start at health 1000
+    - Characters start with:
+        health 1000
+        level 1
+        is alive
+    - Characters can Deal Damage to Characters.
+      - Damage is subtracted from Health
 
 Scenario: Create a character
   When I create a character
   Then the characters health should be 1000
   And the characters level should be 1
   And the character should be alive
+
+Scenario: Attack a character
+  Given characters Bill, Ben have been created
+  When Bill attacks Ben with 5 damage
+  Then Ben's health should be 995
--- features/step_definitions/combat_steps.js
+++ features/step_definitions/combat_steps.js
@@ -1,22 +1,37 @@
-const assert = require('assert')
-const { Before, Given, When, Then } = require('cucumber')
-const {character} = require('../../lib/Character')
-const {expect} = require('chai')
+const assert = require("assert");
+const { Before, Given, When, Then } = require("cucumber");
+const { character } = require("../../lib/Character");
+const { expect } = require("chai");
 
 var myCharacter;
 
-When('I create a character', function () {
-  myCharacter = character()
+var characters = {};
+
+When("I create a character", function () {
+  myCharacter = character();
 });
 
-Then('the characters health should be {int}', function (health) {
-  expect(myCharacter.health).to.equal(health)
+Then("the characters health should be {int}", function (health) {
+  expect(myCharacter.health).to.equal(health);
 });
 
-Then('the characters level should be {int}', function (level) {
-  expect(myCharacter.level).to.equal(level)
+Then("the characters level should be {int}", function (level) {
+  expect(myCharacter.level).to.equal(level);
 });
 
-Then('the character should be alive', function () {
-  expect(myCharacter.isAlive).to.equal(true)
+Then("the character should be alive", function () {
+  expect(myCharacter.isAlive).to.equal(true);
+});
+
+Given("characters Bill, Ben have been created", function () {
+  characters.Bill = character();
+  characters.Ben = character();
+});
+
+When("Bill attacks Ben with {int} damage", function (damage) {
+  characters.Bill.attack({ target: characters.Ben, damage });
+});
+
+Then("Ben's health should be {int}", function (health) {
+  expect(characters.Ben.health).to.equal(health)
 });
