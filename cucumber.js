50af9b0d1445531b9dcaac53c7bafcaf5e832671
temyers <tim.myerscough@mechanicalrock.io>
2020-09-24 17:01:15 +0800

    cleanup

--- cucumber.js
+++ cucumber.js
@@ -3,7 +3,7 @@ let common = [
   'features/**/*.feature', // Specify our feature files
   '--require-module ts-node/register/transpile-only', // Load TypeScript module
   '--require features/**/*.ts', // Load step definitions
-  '--require features/**/*.js', // Load step definitions
+  // '--require features/**/*.js', // Load step definitions
   '--format summary', // Load custom formatter
   '--format progress', // Load custom formatter
   // '--format node_modules/cucumber-pretty', // Load custom formatter
