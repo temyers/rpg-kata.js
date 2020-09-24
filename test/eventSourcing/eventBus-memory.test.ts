c6123e916513e36587f067229558607c42969e61
temyers <tim.myerscough@mechanicalrock.io>
2020-09-24 19:44:00 +0800

    implement publish events in order

--- test/eventSourcing/eventBus-memory.test.ts
+++ test/eventSourcing/eventBus-memory.test.ts
@@ -3,8 +3,11 @@ import { InMemoryEventBus } from "../../lib/eventSourcing/eventBus-memory"
 import * as sinon from "sinon"
 import { CloudEvent } from "cloudevents"
 const { use, expect } = require("chai")
-var sinonChai = require("sinon-chai");
+import * as sinonChai  from "sinon-chai";
+import sinonChaiInOrder from 'sinon-chai-in-order';
+
 use(sinonChai)
+use(sinonChaiInOrder)
 
 
 describe("InMemoryEventBus", () => {
@@ -14,25 +17,59 @@ describe("InMemoryEventBus", () => {
     eventBus = new InMemoryEventBus()
   })
 
-  it("should publishes an event to a single observer", async () => {
-    const onEventMock = sinon.fake()
-    const observer:Observer = {
-      onEvent: onEventMock
-    }
+  it("should publish an event to a single observer", async () => {
+    const observer = givenAnObserver(eventBus)
 
-    eventBus.register(observer);
+    const event = await whenAnEventIsPublished(eventBus)
 
-    const event = new CloudEvent({
-      id: "1",
-      source:"test",
-      specversion: "1.0",
-      type: "example",
-      data: {
-        message: "hello world"
-      }
-    })
-    await eventBus.publish(event)
+    thenTheObserverShouldReceiveEventsInOrder(observer, event)
+  })
+  
+  it("should publish an event to a multiple observers", async () => {
+    const observer1 = givenAnObserver(eventBus)
+    const observer2 = givenAnObserver(eventBus)
+    
+    const event = await whenAnEventIsPublished(eventBus)
+    
+    thenTheObserverShouldReceiveEventsInOrder(observer1, event)
+    thenTheObserverShouldReceiveEventsInOrder(observer2, event)
+  })
 
-    expect(onEventMock).to.have.been.called
+  it("should publish events in order", async () => {
+    const observer = givenAnObserver(eventBus)
+    const event1 = await whenAnEventIsPublished(eventBus)
+    const event2 = await whenAnEventIsPublished(eventBus)
+
+    thenTheObserverShouldReceiveEventsInOrder(observer, event1, event2)
+
   })
 })
+
+function thenTheObserverShouldReceiveEventsInOrder(observer: sinon.SinonSpy<any[], any>, ...events: CloudEvent[]){
+
+  const observations = expect(observer).inOrder.to.have.been.calledWith(events[0])
+  events.slice(1).map(e => observations.subsequently.calledWith(e))
+}
+
+async function whenAnEventIsPublished(eventBus: EventBus) {
+  const event = new CloudEvent({
+    id: Math.random().toString(),
+    source: "test",
+    specversion: "1.0",
+    type: "example",
+    data: {
+      message: "hello world"
+    }
+  })
+  await eventBus.publish(event)
+  return event
+}
+
+function givenAnObserver(eventBus: EventBus): sinon.SinonSpy<any[], any> {
+  const mock = sinon.fake()
+  const observer: Observer = {
+    onEvent: mock
+  }
+  eventBus.register(observer);
+  return mock
+}
