03d99dd17125ef18d879b60d3de055abe42eb98f
temyers <tim.myerscough@mechanicalrock.io>
2020-09-24 16:59:51 +0800

    refactor - convert cucumber to use typescript

---
+++ .mocharc.js
@@ -0,0 +1,9 @@
+module.exports={
+"require": [
+  "ts-node/register/transpile-only",
+  "source-map-support/register"
+],
+"recursive": true,
+"extension": ['js', 'ts'],
+"spec": "test/**/*.test{.js,.ts}"
+}
--- cucumber.js
+++ cucumber.js
@@ -1,8 +1,9 @@
 // cucumber.js
 let common = [
-  // 'features/**/*.feature', // Specify our feature files
-  '--require-module ts-node/register', // Load TypeScript module
-  // '--require step-definitions/**/*.ts', // Load step definitions
+  'features/**/*.feature', // Specify our feature files
+  '--require-module ts-node/register/transpile-only', // Load TypeScript module
+  '--require features/**/*.ts', // Load step definitions
+  '--require features/**/*.js', // Load step definitions
   '--format summary', // Load custom formatter
   '--format progress', // Load custom formatter
   // '--format node_modules/cucumber-pretty', // Load custom formatter
